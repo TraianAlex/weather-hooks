e0da5c9194106d56542fa161550740374a5d2ad1
TraianAlex <victor_traian@yahoo.com>
2020-03-04 16:43:37 -0500

    repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './Form';
+import Weather from './Weather';
+
 
 function App() {
+  const [weather, setWeather] = useState([])
+  const APIKEY = '99a9501fdc462c7e2eafc8ef9386952d'
+
+  async function fetchData(e) {
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    e.preventDefault()
+    const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`)
+      .then(res => res.json())
+      .then(data => data)
+      if(city && country) {
+        setWeather({
+          data: apiData,
+          city: apiData.city,
+          country: apiData.sys.country,
+          description: apiData.weather[0].description,
+          temperature: Math.round(apiData.main.temp * 9/5 - 459.67),
+          error:""
+        }
+        )} else {
+          setWeather({
+            data: '',
+            city: '',
+            country: '',
+            description: '',
+            temperature: '',
+            error:"Please Type A City And Country"
+        }
+        )}
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>WEATHER HOOKS</h3>
+      <Form getWeather={fetchData} />
+      <Weather
+      city={weather.city}
+      country={weather.country}
+      description={weather.description}
+      temperature={weather.temperature}
+      error={weather.error}
+      />
+      {console.log(weather.data)}
     </div>
   );
 }
